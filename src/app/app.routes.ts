164a761fd55f04e24ac742fc90c100163177b79f
devam36 <166159741+devam36@users.noreply.github.com>
2026-03-01 17:56:11 -0500

    Agregar ruta home a app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index';
 import { LoginComponent } from './pages/login/login';
+import { HomeComponent } from './pages/home/home';
 
 export const routes: Routes = [
-  { path: '', component: IndexComponent },
+  { path: '', component: HomeComponent },
+  { path: 'home', redirectTo: '/', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'index', redirectTo: '/', pathMatch: 'full' }
+  { path: 'index', component: IndexComponent }
 ];
